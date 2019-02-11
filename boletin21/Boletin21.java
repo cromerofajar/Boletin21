befa9438c977dfd66493de6cd38f170445164d13
cromerofajar <cromerofajar@nautilus7.danielcastelao.org>
2019-02-11 12:19:48 +0100

    Boletin21 arreglo3

--- boletin21/Boletin21.java
+++ boletin21/Boletin21.java
@@ -17,28 +17,28 @@ public class Boletin21 {
         String opcion,opcion2;
         do{
         opcion=PedirDatos.texto("****MENU****\n 1->Añadir\n 2->Visualizar\n 3->Venta\n 4->Dar de baja\n 5->Buscar uno\n 6->Salir\n\nIntroduzca la funcion que desea realizar:");
-        switch(opcion){
-            case "Añadir": obx.añadirLibro(libro);
+        switch(opcion.toLowerCase()){
+            case "añadir": obx.añadirLibro(libro);
                            break;
-            case "Visualizar": Collections.sort(libro);
+            case "visualizar": Collections.sort(libro);
                                opcion2=JOptionPane.showInputDialog(null,"Seleccione modo\n 1->For\n 2->For each\n 3->Iterator\nIntroduzca su opcion:");
-                               switch(opcion2){
-                                    case "For": obx.verFor(libro);
+                               switch(opcion2.toLowerCase()){
+                                    case "for": obx.verFor(libro);
                                                 break;
-                                    case "For each": obx.verForEach(libro);
+                                    case "for each": obx.verForEach(libro);
                                                 break;
-                                    case "Iterator": obx.verIterator(libro);
+                                    case "iterator": obx.verIterator(libro);
                                                 break;
                                     default: JOptionPane.showMessageDialog(null,"Opcion no valida.");
                                 }
                                break;
-            case "Venta": obx.vender( libro);
+            case "venta": obx.vender( libro);
                           break;
-            case "Dar de baja": obx.darDeBaja(libro);
+            case "dar de baja": obx.darDeBaja(libro);
                                 break;
-            case "Buscar uno": obx.buscarUno(libro);
+            case "buscar uno": obx.buscarUno(libro);
                                break;
-            case "Salir":System.exit(0);
+            case "salir":System.exit(0);
             default: opcion=JOptionPane.showInputDialog(null, "Opcion no valida\nDesea volver al menu?");
         
         }
